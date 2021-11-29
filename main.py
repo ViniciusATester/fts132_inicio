efa2b64035cc596b7b94935088a3adde6e9fe0ac
Vinicius <viniciusat.13@gmail.com>
2021-11-29 13:56:11 -0300

    Criação de funçoes das  4 operaçoes aritimeticas, calculos de área e testes unitários respectivos

---
+++ main.py
@@ -0,0 +1,80 @@
+import math
+
+import pytest
+
+#Cozinheiro
+def somar_dois_numeros(num1, num2):
+    return num1 + num2
+def subtrair_dois_numeros(num1, num2):
+    return num1 - num2
+def multiplicar_dois_numeros(num1, num2):
+    return num1 * num2
+def dividir_dois_numeros(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        return 'Não é possivel dividir por zero'
+
+def elevar_um_numero_pelo_outro(num1, num2):
+    return num1 ** num2
+
+def calcular_area_do_quadrado(lado1):
+    return lado1 ** 2
+
+def calcular_area_do_retangulo(base, altura):
+    return base * altura
+
+def calcular_area_do_triangulo(base, altura):
+    return base * altura / 2
+
+def calcular_area_do_circulo(raio):
+    return math.pi * raio**2
+
+
+
+if __name__ == '__main__':
+#Garçom
+    resultado = somar_dois_numeros(7,5)
+    print(f'A soma é {resultado}')
+
+    resultado = subtrair_dois_numeros(7,5)
+    print(f'A subtração é {resultado}')
+
+    resultado = multiplicar_dois_numeros(7,5)
+    print(f'A multiplicação é {resultado}')
+
+    resultado = dividir_dois_numeros(8,2)
+    print(f'A divisão é {resultado}')
+
+    resultado = elevar_um_numero_pelo_outro(2,3)
+    print(f'A exponenciação é {resultado}')
+
+    resultado = calcular_area_do_quadrado(3)
+    print(f'A área do quadrado  é {resultado}')
+
+    resultado = calcular_area_do_retangulo(2,3)
+    print(f'A área do retangulo é {resultado}')
+
+    resultado = calcular_area_do_triangulo(2,3)
+    print(f'A área do triangulo é {resultado}')
+
+    resultado = calcular_area_do_circulo(3)
+    print(f'A area do circulo é {resultado}')
+
+
+#degustador/teste
+'''
+def testar_somar_dois_numeros():
+    # - 1 Etapa: Configura / Prepara
+    # Dados / Valores
+    # Entrada / Input
+    num1 = 8
+    num2 = 9
+    # Saida / Output
+    resultado_esperado = 17
+
+    # - 2 Etapa: Executa
+    resultado_atual = somar_dois_numeros(num1, num2)
+
+    # - 3 Etapa: Confirma / Check / Valida
+    assert resultado_atual == resultado_esperado'''
---
+++ test/unit/test_main.py
@@ -0,0 +1,79 @@
+import pytest
+
+from main import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros, \
+    elevar_um_numero_pelo_outro, calcular_area_do_quadrado, calcular_area_do_retangulo, calcular_area_do_triangulo, \
+    calcular_area_do_circulo
+
+
+def testar_somar_dois_numeros():
+    # - 1 Etapa: Configura / Prepara
+    # Dados / Valores
+    # Entrada / Input
+    num1 = 5
+    num2 = 4
+    # Saida / Output
+    resultado_esperado = 9
+
+    # - 2 Etapa: Executa
+    resultado_atual = somar_dois_numeros(num1, num2)
+
+    # - 3 Etapa: Confirma / Check / Valida
+    assert resultado_atual == resultado_esperado
+
+def testar_subtrair_dois_numeros():
+    num1 = 8
+    num2 = 3
+    resultado_esperado = 5
+    resultado_atual = subtrair_dois_numeros(num1, num2)
+    assert resultado_atual == resultado_esperado
+
+def testar_multiplicar_dois_numeros():
+    num1 = 8
+    num2 = 3
+    resultado_esperado = 24
+    resultado_atual = multiplicar_dois_numeros(num1, num2)
+    assert resultado_atual == resultado_esperado
+
+def testar_dividir_dois_numeros():
+    num1 = 100
+    num2 = 5
+    resultado_esperado = 20
+    resultado_atual = dividir_dois_numeros(num1, num2)
+    assert resultado_atual == resultado_esperado
+
+def testar_elevar_um_numero_pelo_outro():
+    num1 = 2
+    num2 = 5
+    resultado_esperado = 32
+    resultado_atual = elevar_um_numero_pelo_outro(num1, num2)
+    assert resultado_atual == resultado_esperado
+
+def testar_calcular_area_do_quadrado():
+    lado1= 5
+    resultado_esperado = 25
+    resultado_atual = calcular_area_do_quadrado(lado1)
+    assert resultado_atual == resultado_esperado
+
+
+def testar_calcular_area_do_retangulo():
+    base = 2
+    altura = 5
+    resultado_esperado = 10
+    resultado_atual = calcular_area_do_retangulo(base, altura)
+    assert resultado_atual == resultado_esperado
+
+def testar_calcular_area_do_triangulo():
+    base = 2
+    altura = 5
+    resultado_esperado = 5
+    resultado_atual = calcular_area_do_triangulo(base, altura)
+    assert resultado_atual == resultado_esperado
+
+def testar_calcular_area_do_circulo():
+    raio = 3
+    resultado_esperado = 28.274333882308138
+    resultado_atual = calcular_area_do_circulo(raio)
+    assert resultado_atual == resultado_esperado
+
+
+
